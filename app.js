04ee535eb1d96c57fee590d4af41229118aa3c94
Ken Cheng <kenfcheng@gmail.com>
2020-07-24 10:44:56 -0400

    finished adding team.html

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ function addHtml(member) {
         </div>`;
     }
     console.log("adding team member");
-    fs.appendFile("./output/team.html", data, function (err) {
+    fs.appendFile("./team/team.html", data, function (err) {
       if (err) {
         return reject(err);
       }
@@ -179,7 +179,7 @@ function finishHtml() {
 </body>
 </html>`;
 
-  fs.appendFile("./output/team.html", html, function (err) {
+  fs.appendFile("./team/team.html", html, function (err) {
     if (err) {
       console.log(err);
     }
